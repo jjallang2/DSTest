6d8885ecb62d2a98d5fd8abbf4c2776335f68e6a
jjallang2 <sunk9387@gmail.com>
2025-02-24 21:24:38 +0900

    1

---
+++ Source/DSTest/DSTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DSTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DSTEST_API ADSTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
